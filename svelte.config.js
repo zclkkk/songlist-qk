842d5f17a9610e1fb292a88acae572215785cace
zclkkk <zclkkk@outlook.com>
2026-04-29 16:42:23 +0800

    chore: Drop unused configs

--- svelte.config.js
+++ svelte.config.js
@@ -2,9 +2,6 @@ import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
-  compilerOptions: {
-    runes: true
-  },
   preprocess: vitePreprocess(),
   kit: {
     adapter: adapter()
